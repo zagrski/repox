79f4c620abc45fc44e7a03dc76e54b340b7d4895
Jakub Zagórski <git@zagrski.com>
2025-07-11 13:09:14 +0200

    fix: comment currently unfinished code

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let mut log_manager = log::init_log_manager();
     info!("Starting repox...");
     info!("Loading configuration...");
-    let configuration = configuration::init();
-    if true {
-        if let Err(error) = log_manager.enable_debug() {
-            error!("Failed to enable debug logging: {}", error);
-        }
-    } else if let Err(error) = log_manager.disable_debug() {
-        error!("Failed to disable debug logging: {}", error);
-    }
+    // let configuration = configuration::init();
+    // if configuration.debug {
+    //     if let Err(error) = log_manager.enable_debug() {
+    //         error!("Failed to enable debug logging: {}", error);
+    //     }
+    // } else if let Err(error) = log_manager.disable_debug() {
+    //     error!("Failed to disable debug logging: {}", error);
+    // }
 }
